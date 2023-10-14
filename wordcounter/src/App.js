37ab770c9bc31baaec9f9eb75021d9c95abec3b4
jahnavi sai <jahnavisai1234@gmail.com>
2023-10-14 15:41:38 +0530

    wordcounter app

---
+++ wordcounter/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Textform from "./TextForm";
+
+function App() {
+    return(
+        <>
+        <div className='container'>
+            <Textform heading = "Word Counter"/>
+        </div>
+        </>
+    );
+}
+
+export default App;
